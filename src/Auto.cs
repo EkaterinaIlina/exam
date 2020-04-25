3f43e433e793363558dc4636b5792e281f7f7ad6
EkaterinaIlina <51483385+EkaterinaIlina@users.noreply.github.com>
2020-04-25 09:16:58 +0300

    описание класса

--- src/Auto.cs
+++ src/Auto.cs
@@ -11,5 +11,24 @@ namespace src
         string mark= "";
         string model = "";
         decimal price = 0;
+
+        public string Mark
+        {
+            get { return this.mark; }
+            set { this.mark = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public decimal Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+
     }
 }
